67fbac7a8d2dcf0a3442bd65356d6d8112c74500
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 19:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+  //returns the number of blocks given a value
+}
+
+function distanceFromHqInFeet(location) {
+  //returns the number of blocks given a value
+}
