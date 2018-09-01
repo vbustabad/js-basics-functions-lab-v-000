cc38557d10e92c8a0b0613b10298d6ad03d0ba3e
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 20:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ function distanceFromHqInBlocks(street) {
   return result
 }
 
-function distanceFromHqInFeet (someValue) {
-  distanceFromHqInBlocks(someValue);
-  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
-  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+function distanceFromHqInFeet (street) {
+  const Hq = 42;
+
+  let result 
+  result = (distanceFromHqInBlocks(street) * 264)
+  return result
 }
